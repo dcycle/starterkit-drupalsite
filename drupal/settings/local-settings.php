80aafe81039ad6f4e6c1a6618d4840a945ef42a4
alberto56 <albert@mediatribe.net>
2018-12-02 13:28:40 -0500

    Please squash this commit in the pull request

--- drupal/settings/local-settings.php
+++ drupal/settings/local-settings.php
@@ -3,13 +3,14 @@
 $databases['default']['default'] = [
   'database' => 'drupal',
   'username' => 'root',
-  'password' => 'drupal',
+  'password' => getenv('MYSQL_ROOT_PASSWORD'),
   'host' => 'mysql',
   'port' => '3306',
   'driver' => 'mysql',
   'prefix' => '',
   'collation' => 'utf8mb4_general_ci',
 ];
+
 $settings['hash_salt'] = 'whatever';
 
 // Override config, see
