3bb6d1adb5fe8096daaf06a89b21f3edc8cd88a1
alberto56 <albert@mediatribe.net>
2019-12-09 08:49:12 -0500

    Please squash this commit in the pull request

--- scripts/update-starter-database.sh
+++ scripts/update-starter-database.sh
@@ -8,4 +8,9 @@ echo " => Truncating cache tables for a smaller footprint."
 docker-compose exec drupal /bin/bash -c 'echo "show tables" | drush sqlc | grep cache_ | xargs -I {} echo "truncate {};" | drush sqlc'
 echo " => Updating starter db."
 docker-compose exec drupal /bin/bash -c 'drush sql-dump' > ./drupal/scripts/initial.sql
+echo "[info] Adding newline between , and ( making it easier to read code diffs."
+# shellcheck disable=SC1004
+sed -i -e 's/,(/,\
+(/g' ./drupal/scripts/initial.sql
+rm ./drupal/scripts/initial.sql-e
 echo " => Done updating starter db."
