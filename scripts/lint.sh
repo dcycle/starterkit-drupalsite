1050d6bd52699faf23a53049c2bb710852ad9bd8
Albert Albala <albert@mediatribe.net>
2020-12-03 21:04:13 -0500

    Remove temporary linting containers after use

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -8,8 +8,8 @@ echo '...'
 echo '// @codingStandardsIgnoreEnd'
 echo ''
 
-docker run -v "$(pwd)"/drupal/custom-modules:/code dcycle/php-lint --standard=DrupalPractice /code
-docker run -v "$(pwd)"/drupal/custom-modules:/code dcycle/php-lint --standard=Drupal /code
+docker run --rm -v "$(pwd)"/drupal/custom-modules:/code dcycle/php-lint --standard=DrupalPractice /code
+docker run --rm -v "$(pwd)"/drupal/custom-modules:/code dcycle/php-lint --standard=Drupal /code
 echo 'Linting shell scripts'
-docker run -v "$(pwd)":/code dcycle/shell-lint ./scripts/lint.sh
-docker run -v "$(pwd)":/code dcycle/shell-lint ./scripts/https-deploy.sh
+docker run --rm -v "$(pwd)":/code dcycle/shell-lint ./scripts/lint.sh
+docker run --rm -v "$(pwd)":/code dcycle/shell-lint ./scripts/https-deploy.sh
