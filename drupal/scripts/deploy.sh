baa7d625617390e3ba3dcb37cd752e85f3e89418
Albert Albala <albert@mediatribe.net>
2021-01-19 14:12:32 -0500

    Fail gracefully in case the PHP container cannot connect to the database container

--- drupal/scripts/deploy.sh
+++ drupal/scripts/deploy.sh
@@ -14,16 +14,23 @@ if [ -z "$MYSQL_ROOT_PASSWORD" ]; then
   exit 2;
 fi
 
-echo "Will try to connect to MySQL container until it is up. This can take about 15 seconds."
+echo "Will try to connect to MySQL container until it is up. This can take up to 15 seconds if the container has just been spun up."
 OUTPUT="ERROR"
-while [[ "$OUTPUT" == *"ERROR"* ]]
+TRIES=15
+for i in `seq 1 "$TRIES"`;
 do
   OUTPUT=$(echo 'show databases'|{ mysql -h mysql -u root --password="$MYSQL_ROOT_PASSWORD" 2>&1 || true; })
   if [[ "$OUTPUT" == *"ERROR"* ]]; then
-    echo "MySQL container is not available yet. Should not be long..."
-    sleep 2
+    if [ "$i" == "$TRIES" ];then
+      echo "MySQL container after $TRIES tries, with error $OUTPUT. Abandoning. We suggest you reset Docker to factory defaults, then give Docker 6Gb instead of 2Gb RAM in the Resources section of the preferences pane, and try again. If you are still getting an error please open a ticket at https://github.com/dcycle/starterkit-drupal8site/issues with this message and any other information about your environment."
+      exit 1
+    else
+      echo "Try $i of $TRIES. MySQL container is not available yet. Should not be long..."
+      sleep 2
+    fi
   else
     echo "MySQL is up! Moving on..."
+    break
   fi
 done
 
