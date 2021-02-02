c700450b0e2760fc7691cb64167679ba2226271e
Albert Albala <albert@mediatribe.net>
2021-02-02 11:05:46 -0500

    Test running environment on Drupal 9, ensuring there no incompatible projects (#20)

---
+++ scripts/test-running-environment.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Run some checks on a running environment
+#
+set -e
+
+echo " => Displaying status report for info"
+./scripts/docker-compose.sh exec drupal /bin/bash -c "drush status-report"
+echo " => Making sure we do not have incompatible modules"
+./scripts/docker-compose.sh exec drupal /bin/bash -c "! (drush status-report --severity=2 | grep Incompatible)"
+
+echo " => Done running self-tests."
+echo " =>"
