04f4faf5972ad66959659474ebc3706ce1275675
Albert Albala <albert@mediatribe.net>
2021-04-05 08:37:29 -0400

    Allow using versioned and unversioned settings (#28)

---
+++ drupal/settings/local-settings/unversioned.example.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * If you need environment-specific settings, for example API keys, which
+ * you do not want in git, you can create a file called
+ * ./drupal/settings/local-settings/unversioned.php based on this file; it
+ * will be loaded automatically.
+ */
+
+// This is an example.
+$config['my-super-secret-api-key'] = 'princess';
