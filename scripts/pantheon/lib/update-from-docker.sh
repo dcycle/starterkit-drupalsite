8279c2f2ac1dff6bce1101040e15f6fed314e827
Albert Albala <albert@mediatribe.net>
2021-04-11 07:55:03 -0400

    Use correct locations of pantheon directories

--- scripts/pantheon/lib/update-from-docker.sh
+++ scripts/pantheon/lib/update-from-docker.sh
@@ -5,25 +5,17 @@
 set -e
 
 echo "=> About to copy code from Docker to Pantheon"
-echo "=> Removing core from pantheon git"
-rm -rf ./do-not-commit/pantheon/core
-echo "=> Removing vendor from pantheon git"
-rm -rf ./do-not-commit/pantheon/vendor
 echo "=> Removing modules from pantheon git"
-rm -rf ./do-not-commit/pantheon/modules
+rm -rf ./do-not-commit/pantheon/web/modules
 echo "=> Removing themes from pantheon git"
-rm -rf ./do-not-commit/pantheon/themes
+rm -rf ./do-not-commit/pantheon/web/themes
 echo "=> Removing config from pantheon git"
-rm -rf ./do-not-commit/pantheon/sites/default/config
+rm -rf ./do-not-commit/pantheon/config
 
 echo "=> Moving from docker to pantheon"
-echo "=> Moving core"
-docker cp $(./scripts/docker-compose.sh ps -q drupal):/var/www/html/core ./do-not-commit/pantheon/core
-echo "=> Moving vendor"
-docker cp $(./scripts/docker-compose.sh ps -q drupal):/var/www/html/vendor ./do-not-commit/pantheon/vendor
 echo "=> Moving modules"
-docker cp $(./scripts/docker-compose.sh ps -q drupal):/var/www/html/modules ./do-not-commit/pantheon/modules
+docker cp $(./scripts/docker-compose.sh ps -q drupal):/var/www/html/modules ./do-not-commit/pantheon/web/modules
 echo "=> Moving themes"
-docker cp $(./scripts/docker-compose.sh ps -q drupal):/var/www/html/themes ./do-not-commit/pantheon/themes
+docker cp $(./scripts/docker-compose.sh ps -q drupal):/var/www/html/themes ./do-not-commit/pantheon/web/themes
 echo "=> Moving config"
-docker cp $(./scripts/docker-compose.sh ps -q drupal):/var/www/config ./do-not-commit/pantheon/sites/default/config
+docker cp $(./scripts/docker-compose.sh ps -q drupal):/var/www/config ./do-not-commit/pantheon/web/config
