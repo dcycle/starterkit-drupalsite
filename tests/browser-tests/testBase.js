682d4e27baa96f8e49974b9523707d727c52c7a4
Albert Albala <albert@mediatribe.net>
2021-04-13 19:06:04 -0400

    Please squash this commit in the pull request

---
+++ tests/browser-tests/testBase.js
@@ -0,0 +1,47 @@
+const { expect } = require('chai')
+const fs = require('fs')
+const puppeteer = require('puppeteer')
+
+var screenshot = exports.screenshot = async function(page, name, content) {
+  console.log('SCREENSHOT: ./do-not-commit/screenshots/' + name + '.png')
+  console.log('SCREENSHOT: ./do-not-commit/dom-captures/' + name + '.html')
+  await page.screenshot({path: '/artifacts/screenshots/' + name + '.png'})
+  fs.writeFile('/artifacts/dom-captures/' + name + '.html', content, function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    else {
+      console.log('File ' + name + ' has been saved.');
+      result = true
+    }
+    expect(result).to.be.true;
+  });
+}
+
+exports.assertInSourceCode = async function(page, text, filename="") {
+  if (filename == "") {
+    filename = Math.random();
+  }
+  console.log(' Making sure current URL has the text ');
+  console.log('====> ' + text);
+  await screenshot(page, filename, content = await page.content());
+  try {
+    expect(content).to.include(text);
+  }
+  catch (error) {
+    throw "ERROR - the source code in ./do-not-commit/dom-captures/" + filename + ".html does not contain the string " + text;
+  }
+}
+
+exports.showError = async function (error, browser) {
+  console.log('         _______');
+  console.log('        |.-----.|');
+  console.log('        ||x . x||');
+  console.log('        ||_.-._||');
+  console.log('        `--)-(--`');
+  console.log('       __[=== o]___');
+  console.log('      |:::::::::::|');
+  console.log('      `-=========-`()');
+  await browser.close()
+  console.log(error);
+}
--- tests/browser-tests/testLogInAndEdit.js
+++ tests/browser-tests/testLogInAndEdit.js
@@ -1,22 +1,8 @@
 const { expect } = require('chai')
 const fs = require('fs')
+const testBase = require('./testBase.js')
 
 it('It should be possible to log in and edit a node', async function() {
-  var screenshot = async function(page, name, content) {
-    console.log('taking a screenshot ' + name)
-    await page.screenshot({path: '/artifacts/screenshots/' + name + '.png'})
-    console.log('saving state of the DOM (source code)')
-    fs.writeFile('/artifacts/dom-captures/' + name + '.html', content, function(err) {
-      if (err) {
-        return console.log(err);
-      }
-      else {
-        console.log('File ' + name + ' has been saved.');
-        result = true
-      }
-      expect(result).to.be.true;
-    });
-  }
   this.timeout(25000);
   const puppeteer = require('puppeteer')
   const browser = await puppeteer.launch({
@@ -30,24 +16,23 @@ it('It should be possible to log in and edit a node', async function() {
     await page.setViewport({ width: 1280, height: 800 })
     console.log('go to the login page')
     await page.goto('http://drupal/user')
+    await testBase.assertInSourceCode(page, 'Logd in');
     console.log('enter credentials')
     await page.type('[name=name]', process.env.DRUPALUSER)
     await page.type('[name=pass]', process.env.DRUPALPASS)
     await page.keyboard.press('Enter');
     await page.waitForSelector('nav.tabs')
 
-    await screenshot(page, 'user', await page.content());
+    await testBase.screenshot(page, 'user', await page.content());
 
     console.log('go to /node/1/edit')
     await page.goto('http://drupal/node/1/edit')
 
     await page.waitForSelector('#edit-status-value')
-    await screenshot(page, 'node-1-edit', await page.content());
+    await testBase.screenshot(page, 'node-1-edit', await page.content());
   }
   catch (error) {
-    console.log('Exception alert')
-    await browser.close()
-    console.log(error);
+    await testBase.showError(error, browser);
   }
   await browser.close()
 });
