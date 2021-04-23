abffc6c1498e0f8805b32660095392d532f602bb
Albert Albala <albert@mediatribe.net>
2021-04-23 10:08:59 -0400

    Provide example of how to override the dummy SMTP with active SMTP

--- drupal/settings/local-settings/unversioned.example.php
+++ drupal/settings/local-settings/unversioned.example.php
@@ -9,3 +9,11 @@
 
 // This is an example.
 $config['my-super-secret-api-key'] = 'princess';
+
+// Example to override the dummy mail client with your own SMTP server, in
+// this example gmail.
+$config['smtp.settings']['smtp_host'] = 'smtp.gmail.com';
+$config['smtp.settings']['smtp_port'] = 465;
+$config['smtp.settings']['smtp_username'] = 'YOUR_NAME@gmail.com';
+$config['smtp.settings']['smtp_password'] = 'YOUR_PASSWORD';
+$config['smtp.settings']['smtp_protocol'] = 'ssl';
