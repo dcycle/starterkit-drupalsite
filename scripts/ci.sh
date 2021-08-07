cdfa11cb6a2cf0f16067cd8b42795ad02c4eec66
Albert Albala <albert@mediatribe.net>
2021-08-07 15:54:33 -0400

    Please squash this commit in the pull request

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -22,6 +22,6 @@ echo "Accessibility tests"
 ./scripts/a11y-tests.sh
 BACKUPID="$(./scripts/lib/data-unique-dirname.sh)"
 echo "Export test to $BACKUPID"
-./scripts/create-data-backup.sh
+./scripts/create-data-backup.sh "$BACKUPID"
 echo "Import test to $BACKUPID"
 ./scripts/from-data-backup.sh "$BACKUPID"
