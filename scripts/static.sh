834ad1448375d2dafb16d369f9bfa573e3ab58e3
Albert Albala <albert@mediatribe.net>
2021-10-30 10:08:45 -0400

    Please squash this commit in the pull request

--- scripts/static.sh
+++ scripts/static.sh
@@ -14,5 +14,5 @@ echo ''
 docker run --rm \
   -v "$(pwd)"/drupal/custom-modules:/var/www/html/modules/custom \
   -v "$(pwd)"/scripts/lib/phpstan:/phpstan-drupal \
-  dcycle/phpstan-drupal:3 /var/www/html/modules/custom \
+  dcycle/phpstan-drupal:4 /var/www/html/modules/custom \
   -c /phpstan-drupal/phpstan.neon
