f039639171772f3d2d6c657b5c688e42dc3bbc39
Albert Albala <albert@mediatribe.net>
2022-04-05 23:01:22 -0400

    Please squash this commit in the pull request

--- drupal/custom-modules/my_custom_module/src/App.php
+++ drupal/custom-modules/my_custom_module/src/App.php
@@ -20,16 +20,4 @@ class App {
     // Just an example of where you'd implement testable hooks.
   }
 
-  public function someFunction() {
-    $this->anotherFunction(5);
-  }
-
-  public function anotherFunction($int) {
-    if ($int <= 0) {
-      return;
-    }
-    sleep(1);
-    $this->anotherFunction($int - 1);
-  }
-
 }
