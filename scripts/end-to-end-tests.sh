3ccd07e524648fa31840b367d8892c853c8d29f1
Albert Albala <albert@mediatribe.net>
2022-04-12 09:01:06 -0400

    Use email_registration; make it possible to modify themes/contrib and modules/contrib after initial deployment (#47)

--- scripts/end-to-end-tests.sh
+++ scripts/end-to-end-tests.sh
@@ -11,7 +11,7 @@ echo 'Updating password for admin so our testbot knows how to login'
 docker exec "$(./scripts/docker-compose-container.sh drupal)" /bin/bash -c 'drush upwd $(drush uinf --uid=1 --field=name) '"$PASS"
 
 echo 'Running our tests'
-docker run -e DRUPALUSER=admin -e DRUPALPASS="$PASS" --rm -v "$(pwd)"/tests/browser-tests:/app/test \
+docker run -e DRUPALUSER=admin -e DRUPALMAIL="user+1@localhost.localdomain" -e DRUPALPASS="$PASS" --rm -v "$(pwd)"/tests/browser-tests:/app/test \
   --network starterkit_drupalsite_default \
   -v "$(pwd)"/do-not-commit/screenshots:/artifacts/screenshots \
   -v "$(pwd)"/do-not-commit/dom-captures:/artifacts/dom-captures \
