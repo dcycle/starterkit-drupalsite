077b4cdafc53e1b6ef48cd6b98e0268849e0361f
Albert Albala <albert@mediatribe.net>
2022-04-11 22:58:20 -0400

    Please squash this commit in the pull request

--- tests/browser-tests/testLogInAndEdit.js
+++ tests/browser-tests/testLogInAndEdit.js
@@ -19,7 +19,7 @@ it('It should be possible to log in and edit a node', async function() {
     await page.goto('http://webserver/user')
     await testBase.assertInSourceCode(page, 'Log in');
     console.log('enter credentials')
-    await page.type('[name=name]', process.env.DRUPALUSER)
+    await page.type('[name=name]', process.env.DRUPALMAIL)
     await page.type('[name=pass]', process.env.DRUPALPASS)
     await page.keyboard.press('Enter');
     await page.waitForSelector('nav.tabs')
@@ -28,10 +28,9 @@ it('It should be possible to log in and edit a node', async function() {
 
     console.log('go to /node/1/edit')
     await page.goto('http://webserver/node/1/edit')
-    const content = await page.content();
-    await testBase.screenshot(page, 'node-1-edit', content);
-    console.log(content);
-    await page.waitForSelector('#edit-status-value');
+
+    await page.waitForSelector('#edit-status-value')
+    await testBase.screenshot(page, 'node-1-edit', await page.content());
   }
   catch (error) {
     await testBase.showError(error, browser);
