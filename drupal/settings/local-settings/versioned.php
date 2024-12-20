3a42638816c8415e55a460ec2e8b2848f19a62fe
Albert Albala <albert@mediatribe.net>
2024-12-20 09:54:52 -0500

    Only change the default mail system to SMTP if SMTP actually exists

--- drupal/settings/local-settings/versioned.php
+++ drupal/settings/local-settings/versioned.php
@@ -19,18 +19,21 @@ $settings['hash_salt'] = getenv('HASH_SALT');
 // environment rather than import them for local development.
 $config['stage_file_proxy.settings']['origin'] = 'http://Appstg.prod.acquia-sites.com';
 
-// See https://www.drupal.org/project/smtp/issues/3003639. Overriding
-// smtp_on = TRUE has no effect, but I'm setting it anyway so if someone
-// visits the GUI at http://0.0.0.0:32778/admin/config/system/smtp they
-// will see that SMTP is on in the settings file. What actually causes this
-// to work is the line setting the mail interface to SMTPMailSystem.
-$config['smtp.settings']['smtp_on'] = TRUE;
-// 'mail' is the name of the dummy mailserver defined in docker-compose.yml.
-$config['smtp.settings']['smtp_host'] = 'mail';
-$config['smtp.settings']['smtp_port'] = 1025;
-$config['system.mail']['interface']['default'] = 'SMTPMailSystem';
-$config['system.logging']['error_level'] = 'verbose';
+// Only do this if the module SMTP actually exists to send mail.
+if (class_exists('\Drupal\smtp\Plugin\Mail\SMTPMailSystems')) {
+  // See https://www.drupal.org/project/smtp/issues/3003639. Overriding
+  // smtp_on = TRUE has no effect, but I'm setting it anyway so if someone
+  // visits the GUI at http://0.0.0.0:32778/admin/config/system/smtp they
+  // will see that SMTP is on in the settings file. What actually causes this
+  // to work is the line setting the mail interface to SMTPMailSystem.
+  $config['smtp.settings']['smtp_on'] = TRUE;
+  // 'mail' is the name of the dummy mailserver defined in docker-compose.yml.
+  $config['smtp.settings']['smtp_host'] = 'mail';
+  $config['smtp.settings']['smtp_port'] = 1025;
+  $config['system.mail']['interface']['default'] = 'SMTPMailSystem';
+}
 
+$config['system.logging']['error_level'] = 'verbose';
 $settings['file_private_path'] = '/drupal-private-files';
 
 $config['system.performance']['css']['preprocess'] = FALSE;
